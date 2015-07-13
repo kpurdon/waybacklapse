9b9ae8bdd3c3bf94c168080abb445c4b796a520f
kpurdon <kylepurdon@gmail.com>
2015-07-13 14:44:30 -0600

    Add call to imagemagick-output for GIF creation.

--- wayback/waybacklapse.py
+++ wayback/waybacklapse.py
@@ -7,6 +7,8 @@ import click
 from wayback import Wayback
 
 
+TMP_OUTPUT_DIR = '/images'
+GIF_OUTPUT_DIR = '/output'
 RASTERIZE_SCRIPT = os.environ['rasterize']
 WIDTH = 1024
 RATIO = 4 / 3
@@ -15,7 +17,7 @@ HEIGHT = int(WIDTH / RATIO)
 
 def capture_url(item):
 
-    output_fn = os.path.join('/output', '{0}.png'.format(item[0]))
+    output_fn = os.path.join('/images', '{0}.png'.format(item[0]))
     print('Attemting to download: {0}'.format(item[1]))
     cmd = 'phantomjs {rast} "{url}" {out} "{w}px*{h}px"'
     cmd = cmd.format(rast=RASTERIZE_SCRIPT, url=item[1], out=output_fn, w=WIDTH, h=HEIGHT)
@@ -23,20 +25,30 @@ def capture_url(item):
 
 
 @click.command()
-@click.argument('url')
-@click.option('-b', '--beginning')
-@click.option('-e', '--end')
-def cli(url, beginning, end):
+@click.option('-u', '--url',
+              prompt='What URL would you like to create a timelapse of (ex. google.com)')
+@click.option('-b', '--beginning',
+              prompt='What year (YYYY) would you like your timelapse to start')
+@click.option('-e', '--end',
+              prompt='What year (YYYY) would you like your timelapse to end')
+@click.option('-s', '--speed',
+              prompt='What speed would you like your timelapse (100=slow|25=fast)')
+def cli(url, beginning, end, speed):
     """
-    Waybacklapse:\n
-    \t-b/-beginning: YYYY to begin search\n
-    \t-e/-end: YYYY to end search
+    Generate a GIF of a given website over a given time range.
     """
 
     items = Wayback(url, beginning, end).search()
 
     with Pool(processes=10) as pool:  # not sure how to correctly set this
         pool.map(capture_url, items)
+
+    output_fn = os.path.join(GIF_OUTPUT_DIR, 'waybacklapse.gif')
+    cmd = 'convert -delay {speed} {images} {gif}'
+    cmd = cmd.format(speed=speed, images=os.path.join(TMP_OUTPUT_DIR, '*.png'), gif=output_fn)
+    print(cmd)
+    call(cmd, shell=True)
+
 
 if __name__ == '__main__':
     cli()
